1f302a4c4228773077603b8f9e0156bb01180ae3
Diego Ranon <Mastercelis8891@gmail.com>
2025-05-20 14:19:16 -0400

    Building the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>Diego Ranon</p>
+    </div>
+  )
+}
