a82e6b49ed22ff752c4c0c3ecaaa93993a231ad0
Diego Ranon <Mastercelis8891@gmail.com>
2025-05-23 15:58:49 -0400

    Beginning of TailwindCSS -> Spacing, Borders, and Display

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <div>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Diego Ranon</p>
-    </div>
+      <Username />
+    </header>
   );
 }
