a61bd297b945107fcd6239e3516d1dfc7ae6f8ff
Konan <mastercelis8891@gmail.com>
2025-05-24 14:51:17 -0400

    Styling the order Overview

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Button({ children, disabled, to, type }) {
   const base =
-    "bg-yellow-400 uppercase font-semibold text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    "bg-yellow-400 text-sm uppercase font-semibold text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
